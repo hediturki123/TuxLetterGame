43f98a6ae2fe8c27b60e8370009a498759c21489
hediturki123 <44947373+hediturki123@users.noreply.github.com>
2020-12-03 11:52:18 +0100

    partie 10 done

--- src/game/Jeu.java
+++ src/game/Jeu.java
@@ -1,37 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package game;
 
 import env3d.Env;
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.xml.parsers.ParserConfigurationException;
 import org.lwjgl.input.Keyboard;
-import org.xml.sax.SAXException;
 
 /**
  *
- * @author Alexis YVON, Hedi TURKI SANEKLI
+ * @author gladen
  */
 public abstract class Jeu {
-    private Env env;
-    private Room room;
-    private Profil profil;
-    private Tux tux;
-    private List<Lettre> lettres;
-    private Dico dico;
 
+    enum MENU_VAL {
+        MENU_SORTIE, MENU_CONTINUE, MENU_JOUE
+    }
+
+    private final Env env;
+    private Tux tux;
+    private final Room mainRoom;
+    private final Room menuRoom;
+    private Lettre letter;
+    private Profil profil;
+    private final Dico dico;
+    protected EnvTextMap menuText;                         //text (affichage des texte du jeu)
+    
+    
+    
     public Jeu() {
+
         // Crée un nouvel environnement
         env = new Env();
 
         // Instancie une Room
-        room = new Room();
+        mainRoom = new Room();
+
+        // Instancie une autre Room pour les menus
+        menuRoom = new Room();
+        menuRoom.setTextureEast("textures/black.png");
+        menuRoom.setTextureWest("textures/black.png");
+        menuRoom.setTextureNorth("textures/black.png");
+        menuRoom.setTextureBottom("textures/black.png");
 
         // Règle la camera
         env.setCameraXYZ(50, 60, 175);
@@ -43,112 +54,233 @@ public abstract class Jeu {
         // Instancie un profil par défaut
         profil = new Profil();
         
-        // Instancie les lettres par défaut
-        lettres = new ArrayList<>();
+        // Dictionnaire
+        dico = new Dico("pathtodicofile");
+
+        // instancie le menuText
+        menuText = new EnvTextMap(env);
         
-        // Instancie le dictionnaire.
-        dico = new Dico("src/xml/dico.xml");
+        // Textes affichés à l'écran
+        menuText.addText("Voulez vous ?", "Question", 200, 300);
+        menuText.addText("1. Commencer une nouvelle partie ?", "Jeu1", 250, 280);
+        menuText.addText("2. Charger une partie existante ?", "Jeu2", 250, 260);
+        menuText.addText("3. Sortir de ce jeu ?", "Jeu3", 250, 240);
+        menuText.addText("4. Quitter le jeu ?", "Jeu4", 250, 220);
+        menuText.addText("Choisissez un nom de joueur : ", "NomJoueur", 200, 300);
+        menuText.addText("1. Charger un profil de joueur existant ?", "Principal1", 250, 280);
+        menuText.addText("2. Créer un nouveau joueur ?", "Principal2", 250, 260);
+        menuText.addText("3. Sortir du jeu ?", "Principal3", 250, 240);
     }
-    
+
+    /**
+     * Gère le menu principal
+     *
+     */
     public void execute() {
- 
-        // pour l'instant, nous nous contentons d'appeler la méthode joue comme cela
-        // et nous créons une partie vide, juste pour que cela fonctionne
-        String date = getDate();
-        // il faut décommenter !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        //joue(new Partie(date, mot, niveau));
-         
-        // Détruit l'environnement et provoque la sortie du programme 
+
+        MENU_VAL mainLoop;
+        mainLoop = MENU_VAL.MENU_SORTIE;
+        do {
+            mainLoop = menuPrincipal();
+        } while (mainLoop != MENU_VAL.MENU_SORTIE);
+        this.env.setDisplayStr("Au revoir !", 300, 30);
         env.exit();
     }
+
+
+    // fourni
+    private String getNomJoueur() {
+        String nomJoueur = "";
+        menuText.getText("NomJoueur").display();
+        nomJoueur = menuText.getText("NomJoueur").lire(true);
+        menuText.getText("NomJoueur").clean();
+        return nomJoueur;
+    }
+
     
+    // fourni, à compléter
+    private MENU_VAL menuJeu() {
+
+        MENU_VAL playTheGame;
+        playTheGame = MENU_VAL.MENU_JOUE;
+        Partie partie;
+        do {
+            // restaure la room du menu
+            env.setRoom(menuRoom);
+            // affiche menu
+            menuText.getText("Question").display();
+            menuText.getText("Jeu1").display();
+            menuText.getText("Jeu2").display();
+            menuText.getText("Jeu3").display();
+            menuText.getText("Jeu4").display();
+            
+            // vérifie qu'une touche 1, 2, 3 ou 4 est pressée
+            int touche = 0;
+            while (!(touche == Keyboard.KEY_1 || touche == Keyboard.KEY_2 || touche == Keyboard.KEY_3 || touche == Keyboard.KEY_4)) {
+                touche = env.getKey();
+                env.advanceOneFrame();
+            }
+
+            // nettoie l'environnement du texte
+            menuText.getText("Question").clean();
+            menuText.getText("Jeu1").clean();
+            menuText.getText("Jeu2").clean();
+            menuText.getText("Jeu3").clean();
+            menuText.getText("Jeu4").clean();
+
+            // restaure la room du jeu
+            env.setRoom(mainRoom);
+
+            // et décide quoi faire en fonction de la touche pressée
+            switch (touche) {
+                // -----------------------------------------
+                // Touche 1 : Commencer une nouvelle partie
+                // -----------------------------------------                
+                case Keyboard.KEY_1: // choisi un niveau et charge un mot depuis le dico
+                    // .......... dico.******
+                    // crée un nouvelle partie
+                    partie = new Partie("2018-09-7", "test", 1);
+                    // joue
+                    joue(partie);
+                    // enregistre la partie dans le profil --> enregistre le profil
+                    // .......... profil.******
+                    playTheGame = MENU_VAL.MENU_JOUE;
+                    break;
+
+                // -----------------------------------------
+                // Touche 2 : Charger une partie existante
+                // -----------------------------------------                
+                case Keyboard.KEY_2: // charge une partie existante
+                    partie = new Partie("2018-09-7", "test", 1); //XXXXXXXXX
+                    // Recupère le mot de la partie existante
+                    // ..........
+                    // joue
+                    joue(partie);
+                    // enregistre la partie dans le profil --> enregistre le profil
+                    // .......... profil.******
+                    playTheGame = MENU_VAL.MENU_JOUE;
+                    break;
+
+                // -----------------------------------------
+                // Touche 3 : Sortie de ce jeu
+                // -----------------------------------------                
+                case Keyboard.KEY_3:
+                    playTheGame = MENU_VAL.MENU_CONTINUE;
+                    break;
+
+                // -----------------------------------------
+                // Touche 4 : Quitter le jeu
+                // -----------------------------------------                
+                case Keyboard.KEY_4:
+                    playTheGame = MENU_VAL.MENU_SORTIE;
+            }
+        } while (playTheGame == MENU_VAL.MENU_JOUE);
+        return playTheGame;
+    }
+
+    private MENU_VAL menuPrincipal() {
+
+        MENU_VAL choix = MENU_VAL.MENU_CONTINUE;
+        String nomJoueur;
+
+        // restaure la room du menu
+        env.setRoom(menuRoom);
+
+        menuText.getText("Question").display();
+        menuText.getText("Principal1").display();
+        menuText.getText("Principal2").display();
+        menuText.getText("Principal3").display();
+               
+        // vérifie qu'une touche 1, 2 ou 3 est pressée
+        int touche = 0;
+        while (!(touche == Keyboard.KEY_1 || touche == Keyboard.KEY_2 || touche == Keyboard.KEY_3)) {
+            touche = env.getKey();
+            env.advanceOneFrame();
+        }
+
+        menuText.getText("Question").clean();
+        menuText.getText("Principal1").clean();
+        menuText.getText("Principal2").clean();
+        menuText.getText("Principal3").clean();
+
+        // et décide quoi faire en fonction de la touche pressée
+        switch (touche) {
+            // -------------------------------------
+            // Touche 1 : Charger un profil existant
+            // -------------------------------------
+            case Keyboard.KEY_1:
+                // demande le nom du joueur existant
+                nomJoueur = getNomJoueur();
+                // charge le profil de ce joueur si possible
+                /*if (profil.charge(nomJoueur)) {
+                    choix = menuJeu();
+                } else {
+                    choix = MENU_VAL.MENU_SORTIE;//CONTINUE;
+                }*/
+                break;
+
+            // -------------------------------------
+            // Touche 2 : Créer un nouveau joueur
+            // -------------------------------------
+            case Keyboard.KEY_2:
+                // demande le nom du nouveau joueur
+                nomJoueur = getNomJoueur();
+                // crée un profil avec le nom d'un nouveau joueur
+                profil = new Profil();//nomJoueur);
+                choix = menuJeu();
+                break;
+
+            // -------------------------------------
+            // Touche 3 : Sortir du jeu
+            // -------------------------------------
+            case Keyboard.KEY_3:
+                choix = MENU_VAL.MENU_SORTIE;
+        }
+        return choix;
+    }
+
     public void joue(Partie partie) {
- 
-        // TEMPORAIRE : on règle la room de l'environnement. Ceci sera à enlever lorsque vous ajouterez les menus.
-        env.setRoom(room);
-        
-        // Lecture du dictionnaire.
-        try {
-            dico.lireDictionnaireDOM("src/xml/", "dico.xml");
-        } catch (IOException ex) {
-            Logger.getLogger(Jeu.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SAXException ex) {
-            Logger.getLogger(Jeu.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ParserConfigurationException ex) {
-            Logger.getLogger(Jeu.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        // TEMPORAIRE : récupération d'un mot d'un niveau
-        String mot = dico.getMotDepuisListeNiveau((int) (Math.random()*4)+1);
-        
-        // On instancie la liste de lettres du mot à retrouver.
-        for (int i = 0; i < mot.length(); i++) {
-            Lettre l = new Lettre(mot.charAt(i), (Math.random()*(100-2))+2, (Math.random()*(100-2))+2);
-            lettres.add(l);
-        }
-        
-        // On ajoute les lettres à l'environnement de jeu.
-        lettres.forEach(l -> {
-            env.addObject(l);
-        });
- 
+
         // Instancie un Tux
-        tux = new Tux(env, room);
+        tux = new Tux(env, mainRoom);
         env.addObject(tux);
-         
+
+        letter = new Lettre('a', 10, 10);
+        env.addObject(letter);
+
         // Ici, on peut initialiser des valeurs pour une nouvelle partie
         demarrePartie(partie);
-         
+
         // Boucle de jeu
         Boolean finished;
         finished = false;
         while (!finished) {
- 
+
             // Contrôles globaux du jeu (sortie, ...)
             //1 is for escape key
-            if (env.getKey() == Keyboard.KEY_ESCAPE) {
+            if (env.getKey() == 1) {
                 finished = true;
             }
- 
+
             // Contrôles des déplacements de Tux (gauche, droite, ...)
             tux.deplace();
- 
+
             // Ici, on applique les regles
             appliqueRegles(partie);
- 
+
             // Fait avancer le moteur de jeu (mise à jour de l'affichage, de l'écoute des événements clavier...)
             env.advanceOneFrame();
         }
- 
+
         // Ici on peut calculer des valeurs lorsque la partie est terminée
         terminePartie(partie);
- 
+
     }
-    
-    abstract void demarrePartie(Partie partie);
-    
-    abstract void appliqueRegles(Partie partie);
-    
-    abstract void terminePartie(Partie partie);
-    
-    public double distance (Lettre lettre) {
-        return Math.sqrt (Math.pow(this.tux.getX() - lettre.getX(), 2) + Math.pow(this.tux.getZ() - lettre.getZ(), 2));
-    } 
-    
-    public boolean collision (Lettre lettre) {
-        boolean res = false;
-        if (distance(lettre) < tux.getScale() + lettre.getScale()) {
-            res = true;
-        }
-        return res;
-    }
-    
-    
-    public String getDate() {
-        Date date = Calendar.getInstance().getTime();  
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");  
-        return dateFormat.format(date);
-    }
-    
-    
+
+    protected abstract void demarrePartie(Partie partie);
+
+    protected abstract void appliqueRegles(Partie partie);
+
+    protected abstract void terminePartie(Partie partie);
+
 }
--- src/game/Profil.java
+++ src/game/Profil.java
@@ -1,12 +1,21 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  *
  * @author Alexis YVON, Hedi Turki SANEKLI
  */
 public class Profil {
+    
+    private String nom;
+    private String avatar;
+    private Date anniversaire;
+    private ArrayList<Partie> parties;
 
     public Profil() {
+        parties = new ArrayList<Partie>();
     }
     
 }
